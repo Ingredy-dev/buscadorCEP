90d5d0a46933d558c51aaff32fa2da8ad79d41c2
Ingredy Souza <desouzaingredythais@gmail.com>
2019-10-04 11:03:55 -0300

    busca cep

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component{
+  consultarCep = (event) =>{
+    event.preventDefault();
+
+    let cep = document.getElementById('cep').value;
+    axios.get('https://viacep.com.br/ws/'+cep+'/json/', {})
+      .then(response => {
+        console.log(response)
+        let info = response.data;
+        document.getElementById('dados').innerHTML = '<div id="conteudoEndereco"><p className="infoRua">'+info.logradouro+'</p> <p className="infoEndereco">'+info.bairro+'</p> <p className="infoEndereco">'+info.localidade+'-'+info.uf+'</p> <p className="infoEndereco">'+info.cep+'</p></div>';
+
+      })
+      .catch(error => {
+          // console.log(error)
+          document.getElementById('dados').innerHTML = '<p>Endereço não encontrado</p>';
+      })
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <div className="container">
+          <div id="buscador">
+            <h1>Consultar</h1>
+            <form>
+                <label>cep</label>
+                <input type="text" id="cep" />
+                <button onClick={this.consultarCep}>consultar</button>
+            </form>
+          </div>
+          <div id="dados"></div>
+          </div>
+      </div>
+    );
+  }
+};
 export default App;
